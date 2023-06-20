27f5528a96ebe5fade9b08b39bb0040614a41ec4
David Hurley <34819526+david-hurley@users.noreply.github.com>
2023-06-20 16:47:21 +0000

    added barebones app

---
+++ app/home.py
@@ -0,0 +1,62 @@
+"""Cycle Saviour Home App Page"""
+import streamlit as st
+from streamlit_chat import message
+
+st.set_page_config(layout='wide')
+
+# set the title
+st.markdown("<h1>Find My Stolen Bike!</h1>", unsafe_allow_html=True)
+
+# state variables to hold chat history
+if 'user_query_history' not in st.session_state:
+    st.session_state['user_query_history'] = []
+
+if 'conversation_history' not in st.session_state:
+    st.session_state['conversation_history'] = []
+
+if 'ai_response_history' not in st.session_state:
+    st.session_state['ai_response_history'] = []
+
+# split into columns for chat and responses
+col1, col2 = st.columns([0.7, 0.3])
+
+# this column holds the results once the assistant finds them
+with col1:
+    # this will popup only on second and onward user input but that is okay in our case
+    if st.session_state['user_query_history']:
+
+        # this will ideally be the image, links, title description of add, etc.
+        html_placeholder = '<html>'
+
+        for i in range(0, 5):
+            html_placeholder += '<div style="background-color: rgba(213, 238, 247, 0.5); border-radius: 10px; padding: 10px; margin-bottom: 20px;><table style="border: none;background-color: lightblue; border-radius: 10px;"><tr style="border: none;"><h3><a href="https://google.com" style="text-decoration:none;" target="_blank">"Placeholder"</a></h3></tr><tr style="border: none;">"Placeholder for ad text"<br><br></tr><tr style="border: none;"><strong>"More Placeholder space"</strong><br></tr><br></table></div>'
+        
+        html_placeholder += '</html>'
+
+        st.markdown(html_placeholder, unsafe_allow_html=True)
+
+# this contains the chat interface
+with col2:
+    user_input = st.text_input('Start a chat with our assistant!',
+                               key='input',
+                               label_visibility='visible',
+                               placeholder='My bike was stolen, help me find it!')
+
+    if user_input:
+        if st.session_state['conversation_history']:
+            # placeholder
+            result = None
+        else:
+            # placeholder
+            result = None
+
+        st.session_state.user_query_history.append(user_input)
+        st.session_state.conversation_history.append([(user_input, result)])
+        st.session_state.ai_response_history.append(result)
+
+    # if ai responded then print out dialouge
+    if st.session_state['ai_response_history']:
+
+        for i in range(len(st.session_state['ai_response_history'])-1, -1, -1):
+            message('AI Response Placholder', key=str(i))
+            message(st.session_state['user_query_history'][i], is_user=True, key=str(i) + '_user')
---
+++ app/pages/about.py
@@ -0,0 +1,8 @@
+"""Cycle Saviour About Page"""
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+# set the title
+st.markdown("<h1>About This Page</h1>", unsafe_allow_html=True)
+st.write('This will contain links to GitHub and about the project or just embed GitHub README.md')
