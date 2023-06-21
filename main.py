3611d31a173bc54e91a8233617f01131159f01c2
Conner Schiissler <coschiissler@suncor.com>
2023-06-20 19:36:41 -0600

    reverted to a conversational prompt and added a tool where the user can filter bikes by location

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-from langchain.tools import Tool
-import os
-from langchain.chains.conversation.memory import ConversationBufferWindowMemory
-from langchain.llms import AzureOpenAI
-from langchain.agents import Tool, AgentExecutor, LLMSingleActionAgent, AgentOutputParser, create_json_agent
-import openai
-from langchain.agents.agent_toolkits import JsonToolkit
-from typing import List, Union
-from langchain.prompts import StringPromptTemplate
-from langchain.schema import AgentAction, AgentFinish
-import re
-from langchain import LLMChain
-from langchain.tools.json.tool import JsonSpec
-import requests
+from langchain.agents import Tool
+from langchain.agents import AgentType
+from langchain.memory import ConversationBufferMemory
+from langchain import OpenAI
+from langchain.utilities import SerpAPIWrapper
+from langchain.agents import initialize_agent
 from dotenv import load_dotenv
+import os
+import openai
+from langchain.llms import AzureOpenAI
+from langchain.chat_models import AzureChatOpenAI
 import pinecone
 from sentence_transformers import SentenceTransformer, util
 from PIL import Image
@@ -31,6 +27,9 @@ os.getenv('PINECONE_ENV')
 
 llm = AzureOpenAI(deployment_name="davinci",
                   model_name="text-davinci-003", temperature=0)
+
+chat = AzureChatOpenAI(deployment_name="chat",
+                  model_name="gpt-35-turbo", temperature=0, openai_api_base=os.getenv('OPENAI_API_ENDPOINT'), openai_api_key=os.getenv('OPENAI_API_KEY'), openai_api_version='2023-03-15-preview')
 
 def get_bike_data(a):
     model = SentenceTransformer('clip-ViT-B-32')
@@ -58,123 +57,64 @@ def get_bike_data(a):
     
     return image_links
 
+def greet_user(a):
+    return """Hello, how can I help you? Please provide the following: 
+    - what color is your bike?
+    - what make is your bike?
+    - where was your bike stolen?"""
+
+def get_filtered_results(location):
+    model = SentenceTransformer('clip-ViT-B-32')
+    img_emb = model.encode(Image.open(f'data/images/large_IMG_1704.jpeg')).tolist()
+    pinecone.init(
+    api_key=os.getenv('PINECONE_API_KEY'),
+    environment=os.getenv('PINECONE_ENV')
+    )
+    vdb = pinecone.Index("cycle-saviours")
+    result = vdb.query(
+        vector=img_emb,
+        top_k=3,
+        include_values=False,
+         filter={
+        "ad_location": {"$eq": location}
+        },
+        include_metadata=True
+    )
+    image_links = []
+    
+    matches = result.get('matches', [])
+    for match in matches:
+        metadata = match.get('metadata', {})
+        image_link = metadata.get('image_link')
+        
+        if image_link:
+            image_links.append(image_link)
+    
+    return image_links
+
 
 tools = [
     Tool(
         name="Search Bike Index",
         func=get_bike_data,
         description="Useful when you want to return a list of bike images that match the image uploaded by the user"
+    ),
+     Tool(
+        name="Greet User",
+        func=greet_user,
+        description="A greeting message to the user. Use this tool to start a conversation with the user"
+    ),
+     Tool(
+        name="Search Bike Index by Location",
+        func=get_filtered_results,
+        description="Useful when you want to return a list of bike images and filter by a location. The input is the location of the bike theft. For example, 'calgary'"
     )
 ]
 
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
-class CustomPromptTemplate(StringPromptTemplate):
-    # The template to use
-    template: str
-    # The list of tools available
-    tools: List[Tool]
-
-    def format(self, **kwargs) -> str:
-        # Get the intermediate steps (AgentAction, Observation tuples)
-        # Format them in a particular way
-        intermediate_steps = kwargs.pop("intermediate_steps")
-        thoughts = ""
-        for action, observation in intermediate_steps:
-            thoughts += action.log
-            thoughts += f"\nObservation: {observation}\nThought: "
-        # Set the agent_scratchpad variable to that value
-        kwargs["agent_scratchpad"] = thoughts
-        # Create a tools variable from the list of tools provided
-        kwargs["tools"] = "\n".join(
-            [f"{tool.name}: {tool.description}" for tool in self.tools])
-        # Create a list of tool names for the tools provided
-        kwargs["tool_names"] = ", ".join([tool.name for tool in self.tools])
-        return self.template.format(**kwargs)
-
-
-class CustomOutputParser(AgentOutputParser):
-
-    def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
-        # Check if agent should finish
-        if "Final Answer:" in llm_output:
-            return AgentFinish(
-                # Return values is generally always a dictionary with a single `output` key
-                # It is not recommended to try anything else at the moment :)
-                return_values={"output": llm_output.split(
-                    "Final Answer:")[-1].strip()},
-                log=llm_output,
-            )
-        # Parse out the action and action input
-        regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
-        match = re.search(regex, llm_output, re.DOTALL)
-        if not match:
-            raise ValueError(f"Could not parse LLM output: `{llm_output}`")
-        action = match.group(1).strip()
-        action_input = match.group(2)
-        # Return the action and action input
-        return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
-
-
-output_parser = CustomOutputParser()
-
-# Set up the base template
-template_with_history = """Assistant is designed to be able to assist with finding a users lost/stolen bike
-As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand. 
-Assistant is constantly learning and improving, and its capabilities are constantly evolving. 
-It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses.
-Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions to help with finding a users lost/stolen bike.
-
-Assistant has access to the following tools:
-
-{tools}
-
-Use the following format:
-
-Question: the input question you must answer
-Thought: you should always think about what to do
-Action: the action to take, should be one of [{tool_names}]
-Action Input: the input to the action
-Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat N times)
-Thought: I now know the final answer
-Final Answer: the final answer to the original input question
-
-Begin! Remember to speak as someone trying to find a users lost/stolen bike when giving your final answer.
-
-Previous conversation history:
-{history}
-
-New question: {input}
-{agent_scratchpad}"""
-
-prompt_with_history = CustomPromptTemplate(
-    template=template_with_history,
-    tools=tools,
-    # This omits the `agent_scratchpad`, `tools`, and `tool_names` variables because those are generated dynamically
-    # This includes the `intermediate_steps` variable because that is needed
-    input_variables=["input", "intermediate_steps", "history"]
-)
-
-llm_chain = LLMChain(llm=llm, prompt=prompt_with_history)
-
-tool_names = [tool.name for tool in tools]
-agent = LLMSingleActionAgent(
-    llm_chain=llm_chain,
-    output_parser=output_parser,
-    stop=["\nObservation:"],
-    allowed_tools=tool_names
-)
-
-
-memory = ConversationBufferWindowMemory(k=2)
-
-agent_executor = AgentExecutor.from_agent_and_tools(
-    agent=agent,
-    tools=tools,
-    verbose=True,
-    memory=memory
-)
+agent_chain = initialize_agent(tools, llm, agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION, verbose=True, memory=memory)
 
 while True:
     user_input = input("User: ")
-    agent_executor.run(user_input)
+    agent_chain.run(input=user_input)
