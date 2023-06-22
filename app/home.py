aeae2522fc8a168a50f4ca945ada22128c97e053
David Hurley <34819526+david-hurley@users.noreply.github.com>
2023-06-22 15:46:29 -0600

    Update home.py

--- app/home.py
+++ app/home.py
@@ -1,36 +1,30 @@
 """Cycle Saviour Home App Page"""
 import streamlit as st
-from streamlit_chat import message
 from sentence_transformers import SentenceTransformer
 import pinecone
 import os
-from langchain.tools import Tool
-import os
+from langchain.tools import Tool, StructuredTool
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory, ConversationBufferMemory
-from langchain.llms import AzureOpenAI
 from langchain.agents import Tool, AgentExecutor, LLMSingleActionAgent, AgentOutputParser, initialize_agent, AgentType
-import openai
-from langchain.agents.agent_toolkits import JsonToolkit
 from typing import List, Union
 from langchain.prompts import StringPromptTemplate
 from langchain.schema import AgentAction, AgentFinish
 import re
-from langchain import LLMChain
-from langchain.tools.json.tool import JsonSpec
-import requests
+from langchain import LLMChain, OpenAI
 from dotenv import load_dotenv
-import pinecone
-from sentence_transformers import SentenceTransformer, util
 from PIL import Image
-from langchain.agents.agent_toolkits import AzureCognitiveServicesToolkit
-from azure.storage.blob import BlobServiceClient, BlobClient
-from langchain.tools import StructuredTool
-import io
 from transformers import AutoProcessor, BlipForQuestionAnswering
-from PIL import Image
-import datetime
 
-st.set_page_config(layout='wide')
+####################################################
+# BACKGROUND CONFIGURATIONS AND CACHE
+####################################################
+load_dotenv()
+
+# os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
+
+llm = OpenAI(model_name='text-davinci-003', temperature=0)
+
+st.set_page_config(layout='wide', initial_sidebar_state='collapsed')
 
 @st.cache_resource
 def load_models():
@@ -55,95 +49,60 @@ def load_pinecone(index_name):
 
 index = load_pinecone('cycle-saviours')
 
-# set the title
-st.markdown("<h1>Find My Stolen Bike!</h1>", unsafe_allow_html=True)
-
 if 'conversation_history' not in st.session_state:
     st.session_state['conversation_history'] = []
 
 if 'ai_response_history' not in st.session_state:
     st.session_state['ai_response_history'] = []
 
-# split into columns for chat and responses
-col1, col2 = st.columns([10, 10])
+if 'pinecone_output' not in st.session_state:
+    st.session_state['pinecone_output'] = []
 
-# this column holds the results once the assistant finds them
-with col1:
-    if pinecone_output:
-        html_placeholder = '<html><h6>Ads found online that match your image and description</h6>'
-        for data in pinecone_output:
-            listed_date = str(data['metadata']['list_date']).split('.')[0]
-            html_placeholder += '<div style="background-color: rgba(213, 238, 247, 0.5);' \
-                'border-radius: 10px; padding: 10px; margin-bottom: 20px;><table style="border: none;' \
-                'background-color: lightblue; border-radius: 10px;">' \
-            html_placeholder += '<tr style="border: none;">' \
-                f'<h3><a href={data["metadata"]["ad_link"]} style="text-decoration:none;" target="_blank">{data["metadata"]["ad_title"]}</a></h3>' \
-                f'</tr><tr style="border: none;"><strong>Ad Source: </strong>{data["metadata"]["source"]} <br>' \
-                f'<strong>Ad Location: </strong>{data["metadata"]["ad_location"]} <br>' \
-                f'<strong>Ad List Date: </strong>{listed_date[0:4]+"-"+listed_date[4:6]+"-"+listed_date[6:]}<br>' \
-                f'<br></tr><tr style="border: none;"><img src={data["metadata"]["image_link"]} alt=Ad Id: {data["metadata"]["source_id"]}></tr><br></table></div>'
+####################################################
+# UI CODE
+####################################################
+st.markdown("<h1>Find My Stolen Bike!</h1>", unsafe_allow_html=True)
 
-        html_placeholder += '</html>'
-
-        st.markdown(html_placeholder, unsafe_allow_html=True)
-
-load_dotenv()
-
-openai.api_key = os.getenv('OPENAI_API_KEY')
-openai.api_base = os.getenv('OPENAI_API_ENDPOINT')
-openai.api_type = 'azure'
-openai.api_version = '2023-03-15-preview'
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_ENDPOINT"] = "https://api.langchain.plus"
-os.getenv('LANGCHAIN_API_KEY')
-os.getenv('PINECONE_API_KEY')
-os.getenv('PINECONE_ENV')
-os.getenv("AZURE_COGS_KEY")
-os.getenv("AZURE_COGS_ENDPOINT")
-os.getenv("AZURE_COGS_REGION")
-
-
-llm = AzureOpenAI(deployment_name="davinci",
-                  model_name="text-davinci-003", temperature=0)
-
+col1, col2 = st.columns([0.7, 0.3])
 
 def get_filtered_results(location: str, color: str, bike_model: str):
     """The input is the location of the bike theft and the color of the bike. example: 'calgary', 'Red', 'Trek'"""
     metadata_list = []
-    account_name = "pineconehackathon"
-    account_key = os.getenv("STORAGE_ACCOUNT_KEY")
-    container_name = "container"
 
-    blob_service_client = BlobServiceClient(account_url=f"https://{account_name}.blob.core.windows.net", credential=account_key)
-    container_client = blob_service_client.get_container_client(container_name)
+    img_emb = image_embedding_model.encode(Image.open(file)).tolist()
 
-    for blob in container_client.list_blobs():
-        if blob.name.endswith(('.jpg', '.png', '.jpeg')):
-            blob_client = container_client.get_blob_client(blob.name)
-            blob_data = blob_client.download_blob()
+    result = index.query(
+        vector=img_emb,
+        top_k=10,
+        include_values=False,
+        filter={
+            "ad_location": {"$eq": location}
+        },
+        include_metadata=True
+    )
 
-            with io.BytesIO() as f:
-                blob_data.readinto(f)
-                f.seek(0)
-                img_emb = image_embedding_model.encode(Image.open(f)).tolist()
-
-            result = index.query(
-                vector=img_emb,
-                top_k=3,
-                include_values=False,
-                filter={
-                    "ad_location": {"$eq": location}
-                },
-                include_metadata=True
-            )
-
-            matches = result.get('matches', [])
-            for match in matches:
-                metadata = match.get('metadata', {})
-                metadata_list.append(metadata)
+    matches = result.get('matches', [])
+    for match in matches:
+        metadata = match.get('metadata', {})
+        metadata_list.append(metadata)
 
     return metadata_list
 
+def get_image_text(filepath: str):
+    """Use huggingface model to get image description"""
+    image = Image.open(filepath)
+
+    text = "what color is this bike?"
+    inputs = image_text_processor(images=image, text=text, return_tensors="pt")
+    outputs = image_text_model.generate(**inputs)
+    text_output1 = image_text_processor.decode(outputs[0], skip_special_tokens=True)
+
+    text = "what type of bike is this?"
+    inputs = image_text_processor(images=image, text=text, return_tensors="pt")
+    outputs = image_text_model.generate(**inputs)
+    text_output2 = image_text_processor.decode(outputs[0], skip_special_tokens=True)
+
+    return f'A {text_output1} {text_output2} bike'
 
 structured_tool = StructuredTool.from_function(get_filtered_results)
 
@@ -154,7 +113,6 @@ tools = [
         description="Useful when you want to return a list of bike images and filter by location and color. The input is the location of the bike theft and the color of the bike. example: 'calgary', 'red'",
     )
 ]
-
 
 class CustomPromptTemplate(StringPromptTemplate):
     # The template to use
@@ -247,7 +205,6 @@ agent = LLMSingleActionAgent(
     allowed_tools=tool_names
 )
 
-
 memory = ConversationBufferWindowMemory(k=5)
 
 agent_executor = AgentExecutor.from_agent_and_tools(
@@ -257,20 +214,10 @@ agent_executor = AgentExecutor.from_agent_and_tools(
     memory=memory
 )
 
-toolkit = AzureCognitiveServicesToolkit()
-
-agent = initialize_agent(
-    tools=toolkit.get_tools(),
-    llm=llm,
-    agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
-    verbose=True,
-)
-
 memory = ConversationBufferMemory(memory_key="chat_history")
 
 conversation_agent = initialize_agent(
     [structured_tool], llm, agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True, memory=memory)
-
 
 class ChatBot:
     def __init__(self):
@@ -295,20 +242,6 @@ class ChatBot:
         with open(file_path, "wb") as f:
             f.write(uploaded_file.getbuffer())
 
-        # Upload the file to Azure Blob Storage
-        account_name = "pineconehackathon"
-        account_key = os.getenv("STORAGE_ACCOUNT_KEY")
-        container_name = "container"
-        blob_name = uploaded_file.name
-
-        blob_service_client = BlobServiceClient(
-            account_url=f"https://{account_name}.blob.core.windows.net/", credential=account_key)
-        blob_client = blob_service_client.get_blob_client(
-            container=container_name, blob=blob_name)
-
-        with open(file_path, "rb") as data:
-            blob_client.upload_blob(data, overwrite=True)
-
         return file_path
 
     def ask_file(self):
@@ -322,22 +255,41 @@ class ChatBot:
         else:
             return None
 
-
 with col2:
     chatbot = ChatBot()
     file = chatbot.ask_file()
     city = st.selectbox('Please select a city (REQUIRED)', ['SELECT CITY', 'calgary', 'edmonton', 'vancouver'])
     if file and city:
-        bike_image_analysis = agent.run(
-            "what color is this bike? what model is this bike?"
-            "https://pineconehackathon.blob.core.windows.net/container/large_IMG_1704.jpeg"
-        )
+        bike_image_analysis = get_image_text(file)
         st.text(f"AI: Does this sound like your bike? {bike_image_analysis}")
         user_response = st.text_input('User Response:')
         if user_response.lower() == 'yes':
             output = conversation_agent.run(
                 f"The user has had their bike stolen from them, please help them find it. DESCRIPTION: {bike_image_analysis} LOCATION: {city}. Once you have the links for the ad STOP & provide them the links to the ads.")
-            pinecone_output = get_filtered_results(city, "black", "Trek")
+            st.session_state['pinecone_output'] = get_filtered_results(city, "black", "Trek")
+            # this column holds the results once the assistant finds them
+            with col1:
+                if st.session_state['pinecone_output']:
+                    html_placeholder = '<html><h6>Ads found online that match your image and description</h6>'
+                    for data in st.session_state['pinecone_output']:
+
+                        listed_date = str(data['list_date']).split('.')[0]
+
+                        html_placeholder += '<div style="background-color: rgba(213, 238, 247, 0.5);' \
+                            'border-radius: 10px; padding: 10px; margin-bottom: 20px;><table style="border: none;' \
+                            'background-color: lightblue; border-radius: 10px;">'
+                        
+                        html_placeholder += '<tr style="border: none;">' \
+                            f'<h3><a href={data["ad_link"]} style="text-decoration:none;" target="_blank">{data["ad_title"]}</a></h3>' \
+                            f'</tr><tr style="border: none;"><strong>Ad Source: </strong>{data["source"]} <br>' \
+                            f'<strong>Ad Location: </strong>{data["ad_location"]} <br>' \
+                            f'<strong>Ad List Date: </strong>{listed_date[0:4]+"-"+listed_date[4:6]+"-"+listed_date[6:]}<br>' \
+                            f'<br></tr><tr style="border: none;"><img src={data["image_link"]} alt=Ad Id: {data["source_id"]}></tr><br></table></div>'
+
+                    html_placeholder += '</html>'
+
+                    st.markdown(html_placeholder, unsafe_allow_html=True)
+                    
             st.session_state.conversation_history.append(("AI:", output))
             st.session_state.ai_response_history.append(output)
     else:
